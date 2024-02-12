84f865024cbe99b95b5d350aa20a4152d2fba416
Lilly <archaic.archea@gmail.com>
2024-02-11 21:29:19 -0500

    Init

---
+++ src/camera.rs
@@ -0,0 +1,160 @@
+use cgmath::{Angle, Euler, SquareMatrix, Vector3, Vector4};
+use winit::{event::KeyEvent, keyboard::{KeyCode, PhysicalKey}};
+
+
+#[rustfmt::skip]
+pub const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::new(
+    1.0, 0.0, 0.0, 0.0,
+    0.0, 1.0, 0.0, 0.0,
+    0.0, 0.0, 0.5, 0.5,
+    0.0, 0.0, 0.0, 1.0,
+);
+
+pub struct Camera {
+    pub dir: Euler<cgmath::Deg<f32>>,
+    pub pos: Vector3<f32>,
+    pub aspect: f32,
+    pub fovy: f32,
+    pub znear: f32,
+    pub zfar: f32,
+}
+
+impl Camera {
+    fn build_view_projection_matrix(&self) -> cgmath::Matrix4<f32> {
+        let mut view = cgmath::Matrix4::from(self.dir);
+        view.w = Vector4::new(self.pos.x, self.pos.y, self.pos.z, 1.0);
+        view = view.invert().unwrap();
+        let proj = cgmath::perspective(cgmath::Deg(self.fovy), self.aspect, self.znear, self.zfar);
+        proj * view
+    }
+}
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
+pub struct CameraUniform {
+    view_proj: [[f32; 4]; 4],
+}
+
+impl CameraUniform {
+    pub fn new() -> Self {
+        use cgmath::SquareMatrix;
+        Self {
+            view_proj: cgmath::Matrix4::identity().into(),
+        }
+    }
+
+    pub fn update_view_proj(&mut self, camera: &Camera) {
+        self.view_proj = (OPENGL_TO_WGPU_MATRIX * camera.build_view_projection_matrix()).into();
+    }
+}
+
+pub struct CameraController {
+    speed: f32,
+    is_up_pressed: bool,
+    is_down_pressed: bool,
+    is_forward_pressed: bool,
+    is_backward_pressed: bool,
+    is_left_pressed: bool,
+    is_right_pressed: bool,
+}
+
+impl CameraController {
+    pub fn new(speed: f32) -> Self {
+        Self {
+            speed,
+            is_up_pressed: false,
+            is_down_pressed: false,
+            is_forward_pressed: false,
+            is_backward_pressed: false,
+            is_left_pressed: false,
+            is_right_pressed: false,
+        }
+    }
+
+    pub fn process_events(&mut self, event: &crate::WindowEvent) -> bool {
+        match event {
+            crate::WindowEvent::KeyboardInput {
+                event: KeyEvent {
+                    physical_key: PhysicalKey::Code(keycode),
+                    state,
+                    ..
+                },
+                ..
+            } => {
+                let is_pressed = state.is_pressed();
+                match keycode {
+                    KeyCode::Space => {
+                        self.is_up_pressed = is_pressed;
+                        true
+                    }
+                    KeyCode::ShiftLeft => {
+                        self.is_down_pressed = is_pressed;
+                        true
+                    }
+                    KeyCode::KeyW | KeyCode::ArrowUp => {
+                        self.is_forward_pressed = is_pressed;
+                        true
+                    }
+                    KeyCode::KeyA | KeyCode::ArrowLeft => {
+                        self.is_left_pressed = is_pressed;
+                        true
+                    }
+                    KeyCode::KeyS | KeyCode::ArrowDown => {
+                        self.is_backward_pressed = is_pressed;
+                        true
+                    }
+                    KeyCode::KeyD | KeyCode::ArrowRight => {
+                        self.is_right_pressed = is_pressed;
+                        true
+                    }
+                    _ => false,
+                }
+            }
+            _ => false,
+        }
+    }
+
+    pub fn update_camera(&self, camera: &mut Camera) {
+        let forward = Vector3::new(
+            camera.dir.z.cos() * camera.dir.x.sin(), 
+            -camera.dir.z.sin(), 
+            camera.dir.z.cos() * camera.dir.x.cos()
+        );
+
+        if self.is_forward_pressed {
+            camera.pos += forward * self.speed;
+        } else if self.is_backward_pressed {
+            camera.pos -= forward * self.speed;
+        }
+
+        /*use cgmath::InnerSpace;
+        let forward = camera.target - camera.eye;
+        let forward_norm = forward.normalize();
+        let forward_mag = forward.magnitude();
+
+        // Prevents glitching when camera gets too close to the
+        // center of the scene.
+        if self.is_forward_pressed && forward_mag > self.speed {
+            camera.eye += forward_norm * self.speed;
+        }
+        if self.is_backward_pressed {
+            camera.eye -= forward_norm * self.speed;
+        }
+
+        let right = forward_norm.cross(camera.up);
+
+        // Redo radius calc in case the up/ down is pressed.
+        let forward = camera.target - camera.eye;
+        let forward_mag = forward.magnitude();
+
+        if self.is_right_pressed {
+            // Rescale the distance between the target and eye so
+            // that it doesn't change. The eye therefore still
+            // lies on the circle made by the target and eye.
+            camera.eye = camera.target - (forward + right * self.speed).normalize() * forward_mag;
+        }
+        if self.is_left_pressed {
+            camera.eye = camera.target - (forward - right * self.speed).normalize() * forward_mag;
+        }*/
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,170 @@
+use cgmath::{Deg, Euler};
+use render::Vertex;
+use winit::{
+    dpi::Position, event::{ElementState, Event, KeyEvent, WindowEvent}, event_loop::{ControlFlow, EventLoop}, keyboard::{KeyCode, PhysicalKey}, window::WindowBuilder
+};
+
+mod render;
+mod texture;
+mod camera;
+
+fn main() {
+    let (obj, _) = tobj::load_obj(
+        "./utahteapot.obj", 
+        &tobj::LoadOptions {
+            single_index: true,
+            triangulate: true,
+            ignore_lines: true,
+            ignore_points: true,
+        },
+    ).unwrap();
+    let obj = obj[0].clone();
+
+    let vert = obj.mesh.positions;
+    let tex = obj.mesh.texcoords;
+    let indicies = obj.mesh.indices.iter().map(|val| {*val as u16}).collect();
+
+    let mut verticies = Vec::new();
+
+    for index in 0..(vert.len() / 3) {
+        let index = index * 3;
+        let vert = [vert[index], vert[index + 1], vert[index + 2]];
+        let tex = if tex.len() != 0 {
+            [tex[index], tex[index + 1]]
+        } else {
+            [0.0, 0.0]
+        };
+
+        verticies.push(Vertex {
+            position: vert,
+            tex_coords: tex,
+        })
+    }
+
+    let mut vert = render::VERTICES.lock().unwrap();
+    *vert = verticies;
+    let mut idx = render::INDICES.lock().unwrap();
+    *idx = indicies;
+
+    std::mem::drop(vert);
+    std::mem::drop(idx);
+
+    /*let thread = std::thread::spawn(|| {
+        loop {
+            let cur_time = std::time::Instant::now();
+
+            render::VERTICES.lock().unwrap()[0].position[0] += 0.002;
+            
+
+            let end_time = cur_time.elapsed();
+            let sleep_time = 0.01 - end_time.as_secs_f64();
+
+            std::thread::sleep(std::time::Duration::from_secs_f64(sleep_time));
+        }
+    });*/
+
+    let event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new().with_title("GPU Test (Alt to drop mouse)").build(&event_loop).unwrap();
+
+    let state = pollster::block_on(render::RenderState::new(&window));
+
+    win_loop(state, event_loop);
+}
+
+pub fn win_loop(mut state: render::RenderState, event_loop: EventLoop<()>) {
+    let sensitivity = 1.0;
+
+    let mut cursor_in: Option<winit::event::DeviceId> = None;
+    let mut cursor_pos: Option<winit::dpi::PhysicalPosition<f64>> = None;
+    let mut cursor_reset = false;
+
+    event_loop.run(move |event, targ| {
+        match event {
+            Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == state.window().id() => {
+                if !state.input(event) {
+                    match event {
+                        WindowEvent::CursorEntered { device_id } => {
+                            if cursor_in == None {
+                                state.window().set_cursor_grab(winit::window::CursorGrabMode::Confined).unwrap();
+                                //state.window().set_cursor_visible(false);
+                                println!("Cursor in");
+                                cursor_in = Some(device_id.clone());
+                                cursor_pos = None;
+                            }
+                        },
+                        WindowEvent::CursorLeft { device_id } => {
+                            /*if Some(device_id) == cursor_in.as_ref() {
+                                //state.window().set_cursor_visible(true);
+                                println!("Cursor out");
+                                cursor_in = None;
+                                cursor_pos = None;
+                            }*/
+                        },
+                        WindowEvent::CursorMoved { device_id, position } => {
+                            if Some(device_id) == cursor_in.as_ref() {
+                                let old_pos = cursor_pos.unwrap_or(position.clone());
+                                let new_pos = position.clone();
+
+                                cursor_pos = Some(new_pos.clone());
+
+                                let rel_x = ((new_pos.x - old_pos.x) * -sensitivity) as f32;
+                                let rel_y = ((new_pos.y - old_pos.y) * sensitivity) as f32;
+
+                                let e = Euler::new(
+                                    Deg(rel_y), 
+                                    Deg(rel_x),
+                                    Deg(0.0),
+                                );
+
+                                state.cam_dir(e);
+                            }
+                        }
+                        WindowEvent::CloseRequested
+                        | WindowEvent::KeyboardInput {
+                            event: KeyEvent {
+                                physical_key: PhysicalKey::Code(KeyCode::Escape),
+                                ..
+                            },
+                            ..
+                        } => targ.exit(),
+                        WindowEvent::KeyboardInput { 
+                            event: KeyEvent {
+                                physical_key: PhysicalKey::Code(KeyCode::AltLeft),
+                                ..
+                            },
+                            ..
+                        } => {
+                            state.window().set_cursor_grab(winit::window::CursorGrabMode::None);
+                        }
+                        WindowEvent::Resized(physical_size) => {
+                            state.resize(*physical_size);
+                        }
+                        WindowEvent::ScaleFactorChanged { scale_factor, inner_size_writer } => {
+                            state.resize(state.window().inner_size());
+                        }
+                        WindowEvent::RedrawRequested => {
+                            state.update();
+                            match state.render() {
+                                Ok(_) => {}
+                                // Reconfigure the surface if it's lost or outdated
+                                Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
+                                    state.resize(state.size)
+                                }
+                                // The system is out of memory, we should probably quit
+                                Err(wgpu::SurfaceError::OutOfMemory) => targ.exit(),
+                                // We're ignoring timeouts
+                                Err(wgpu::SurfaceError::Timeout) => (),
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+            },
+            Event::AboutToWait => state.window().request_redraw(),
+            _ => {}
+        }
+    }).unwrap();
+}
